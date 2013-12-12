80aba0d53b4de955ddb0fc900cd1171c7ecc8457
Sean Chu <sean1985zc2@gmail.com>
2013-12-12 11:43:49 +0000

    Update keypress.py

--- bin/keypress.py
+++ bin/keypress.py
@@ -1,22 +1,30 @@
 #!/usr/bin/env python 
-## A tiny, nifty script for playing musical notes on each keypress. 
-## 
-##	Copyright Sayan "Riju" Chakrabarti (sayanriju) 2009 
-##	me[at]sayanriju[dot]co[dot]cc ## 
-##		Released under WTFPL Version 2 
-## (DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE) 
-##	Copy of license text can be found online at ##		http://sam.zoy.org/wtfpl/COPYING 
-## http://rants.sayanriju.co.cc/script-to-make-tick-tick-sound-on-keypress
+## Play sounds on keypresses in Linux
+## Originally by Sayan "Riju" Chakrabarti (sayanriju) 
+## http://rants.sayanriju.co.cc/script-to-make-tick-tick-sound-on-keypres
  
 from Xlib.display import Display 
+import threading
 import os 
 import time
 
-notes=[440,494,523,587,659,698,784]	 
+notes=[440,494,523,587,659,698,784]
  
-ZERO,SHIFT,ALT,CTL=[],[],[],[] 
+ZERO,SHIFT,ALT,CTL,BACK,SPACE,ENTER,SC=[],[],[],[],[],[],[],[] 
 for i in range(0,32): 
 	ZERO.append(0) 
+	if i==4: 
+		ENTER.append(16) 
+	else: 
+		ENTER.append(0)
+	if i==8: 
+		SPACE.append(2) 
+	else: 
+		SPACE.append(0)
+	if i==2: 
+		BACK.append(64) 
+	else: 
+		BACK.append(0)
 	if i==6: 
 		SHIFT.append(4) 
 	else: 
@@ -29,18 +37,48 @@ for i in range(0,32):
 		ALT.append(1) 
 	else: 
 		ALT.append(0) 
+	if i==6: 
+		SC.append(4)
+	elif i==4:
+		SC.append(32)
+	else: 
+		SC.append(0) 
 		 
-ignorelist=[ZERO,ALT,SHIFT,CTL] 
+ignorelist=[ZERO,ALT,SHIFT,CTL,SC] #
+# ignorelist=[ZERO]
 
+class KeyPress(threading.Thread):
+	def run(self):
+		os.system("aplay /local/cz12g09/Software/keypress/sounds/keyn.wav")
+
+class BackKeyPress(threading.Thread):
+	def run(self):
+		os.system("aplay /local/cz12g09/Software/keypress/sounds/Back.wav")
+
+class SpaceKeyPress(threading.Thread):
+	def run(self):
+		os.system("aplay /local/cz12g09/Software/keypress/sounds/Space.wav")
+		
+class EnterKeyPress(threading.Thread):
+	def run(self):
+		os.system("aplay /local/cz12g09/Software/keypress/sounds/typewriterding.wav")
+		
 def main(): 
-	disp = Display()	# connect to display	 
-
-	while 1:	#event loop 
+	disp = Display()
+	while 1:
 		keymap = disp.query_keymap() 
 		if keymap not in ignorelist: 
-			os.popen("curl http://localhost:4567/key")
-			time.sleep(0.1)
-
+			if keymap==BACK:
+				BackKeyPress().start()
+			elif keymap==SPACE:
+				SpaceKeyPress().start()
+			elif keymap==ENTER:
+				EnterKeyPress().start()
+			else:
+				KeyPress().start()
+			time.sleep(0.2)
+		else:
+			time.sleep(0.02)
  
 if __name__ == '__main__': 
 	main() 
